74296070b64137f57aee22e28a002d6b105c927f
YAMJALA MANI SANDEEP <yamjalamanisandeep@gmail.com>
2025-03-04 18:21:43 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,30 +1,3 @@
-// 'use client'
-// import About from "./components/About";
-// import Certificates from "./components/Certificates";
-// import Contact from "./components/Contact";
-// import Header from "./components/Header";
-// import Navbar from "./components/Navbar";
-// import Projects from "./components/Projects";
-// import Skills from "./components/Skills";
-
-
-
-// export default function Home() {
-//   return (
-//     <>
-//     <Navbar/>
-//     <Header/>
-//     <About/>
-//     <Skills/>
-//     <Projects/>
-//     <Certificates/>
-//     <Contact/>
-//     </>
-//   );
-// }
-
-
-
 'use client';
 import About from "./components/About";
 import Certificates from "./components/Certificates";
