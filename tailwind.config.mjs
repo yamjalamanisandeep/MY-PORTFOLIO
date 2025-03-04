3358e6b4c507b780bd86e817f4252b9cc4713e66
YAMJALA MANI SANDEEP <yamjalamanisandeep@gmail.com>
2025-03-04 18:22:25 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,4 @@
-  // import { Outfit } from 'next/font/google'
 
-  // /** @type {import('tailwindcss').Config} */
-  // export default {
-  //   content: [
-  //     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  //     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  //     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  //   ],
-  //   theme: {
-  //     extend: {
-  //       colors: {
-  //         lightHover:"#fcf4ff",
-  //         darkHover:"#2a004a",
-  //         darkTheme:"#11001F",
-  //       },
-  //       fontfamily:{
-  //         Outfit:["Outfit","sans-serif"],
-  //         Ovo:["Ovo","serif"]
-  //       }
-  //     },
-  //   },
-  //   plugins: [],
-  // };
 
 /** @type {import('tailwindcss').Config} */
 export default {
